4722094368ad0cf119e6a602a8277fef54b2241e
HoyeongJeon <hoyeong0302@gmail.com>
2021-08-06 10:46:00 +0900

    day16 done.

--- src/pages/Days/Day15.tsx
+++ src/pages/Days/Day15.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import waytoaiport from "./pictures/Day15/waytoaiport.jpg";
-import before1 from "./pictures/Day15/before1.jpg";
-import before2 from "./pictures/Day15/before2.jpg";
-import selfie from "./pictures/Day15/selfie.jpg";
-import watpho1 from "./pictures/Day15/watpho1.jpg";
-import watpho2 from "./pictures/Day15/watpho2.jpg";
-import watpho3 from "./pictures/Day15/watpho3.jpg";
-import buddha from "./pictures/Day15/buddha.jpg";
-import bigfoot from "./pictures/Day15/bigfoot.jpg";
-import beers from "./pictures/Day15/beers.jpg";
+import waytoaiport from "./pictures/day15/waytoaiport.jpg";
+import before1 from "./pictures/day15/before1.jpg";
+import before2 from "./pictures/day15/before2.jpg";
+import selfie from "./pictures/day15/selfie.jpg";
+import watpho1 from "./pictures/day15/watpho1.jpg";
+import watpho2 from "./pictures/day15/watpho2.jpg";
+import watpho3 from "./pictures/day15/watpho3.jpg";
+import buddha from "./pictures/day15/buddha.jpg";
+import bigfoot from "./pictures/day15/bigfoot.jpg";
+import beers from "./pictures/day15/beers.jpg";
 // import aiport1 from "./pictures/Day15/aiport1.mp4";
 import "./Day15.css";
 
@@ -17,14 +17,14 @@ const Day15 = () => {
     <>
       <div className="DayContainer">
         <span>190315</span>
-        <div className="Day15Container__box">
+        <div className="DayContainer__box">
           <img src={waytoaiport}></img>
           <img src={before1}></img>
           <img src={before2}></img>
           <img src={selfie}></img>
           <img src={watpho1}></img>
         </div>
-        <div className="Day15Container__box">
+        <div className="DayContainer__box">
           <img src={watpho2}></img>
           <img src={watpho3}></img>
           <img src={buddha}></img>
--- src/pages/Days/Day16.tsx
+++ src/pages/Days/Day16.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import morning from "./pictures/day16/morning.jpg";
+import morning2 from "./pictures/day16/morning2.jpg";
+import street from "./pictures/day16/street.jpg";
+import selfie from "./pictures/day16/selfie.jpg";
+import shit from "./pictures/day16/shit.jpg";
+import mikol from "./pictures/day16/mikol.jpg";
+import waqqu from "./pictures/day16/waqqu.jpg";
+import siam from "./pictures/day16/siam.jpg";
+import trash from "./pictures/day16/trash.jpg";
+import nightselfie from "./pictures/day16/nightselfie.jpg";
+import popup from "./pictures/day16/popup.jpg";
+import temple from "./pictures/day16/temple.jpg";
+import lotti from "./pictures/day16/lotti.jpg";
+// import aiport1 from "./pictures/Day15/aiport1.mp4";
+import "./Day15.css";
 
 const Day16 = () => {
-  return <div>16</div>;
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const picRef = useRef(null);
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    // const picRef = useRef(null);
+    setIsClicked(!isClicked);
+    if (isClicked == true) {
+      e.target.classList.add("hello");
+    }
+    console.log(isClicked);
+  };
+  return (
+    <>
+      <div className="DayContainer">
+        <span>190316</span>
+        <div
+          onClick={handleClick}
+          className={isClicked ? "clickedBox" : "clickedBox noshow"}
+        >
+          <img className="clickedBox__pic" src={popup}></img>
+        </div>
+        <div className="DayContainer__box">
+          <img src={morning}></img>
+          <img src={morning2}></img>
+          <img src={street}></img>
+          <img src={selfie}></img>
+          <img src={shit}></img>
+          <img src={mikol}></img>
+        </div>
+        <div className="DayContainer__box">
+          <img src={waqqu}></img>
+          <img src={siam}></img>
+          <img src={trash}></img>
+          <img
+            src={nightselfie}
+            onClick={handleClick}
+            className={isClicked ? "noclicked" : "clicked"}
+            ref={() => picRef}
+          ></img>
+          <img src={temple}></img>
+          <img src={lotti} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Day16;
